77280cb87a32868138671b339069ef08eb96854e
udj13 <shlyagin@gmail.com>
2025-03-13 20:23:54 +0300

    Set NMEA output frequency 10Hz

--- src/gps.rs
+++ src/gps.rs
@@ -1,20 +1,20 @@
 use colored::*;
 use esp_idf_hal::delay::BLOCK;
 use esp_idf_hal::gpio;
-use esp_idf_hal::gpio::{AnyIOPin};
+use esp_idf_hal::gpio::AnyIOPin;
 use esp_idf_hal::prelude::Hertz;
-use esp_idf_hal::uart::{UART1, UartDriver};
+use esp_idf_hal::uart::{UartDriver, UART1};
 use std::sync::mpsc;
 use std::sync::mpsc::Receiver;
+use std::thread;
+use std::time::Duration;
 
 pub struct Gps {
     pub rx: Receiver<String>,
 }
 
 impl Gps {
-    pub fn new(uart1: UART1, tx:AnyIOPin, rx: AnyIOPin) -> Gps {
-
-
+    pub fn new(uart1: UART1, tx: AnyIOPin, rx: AnyIOPin) -> Gps {
         println!("Starting UART");
         let config = esp_idf_hal::uart::config::Config::new().baudrate(Hertz(115_200));
         let uart = UartDriver::new(
@@ -24,8 +24,57 @@ impl Gps {
             Option::<gpio::Gpio0>::None,
             Option::<gpio::Gpio1>::None,
             &config,
-        ).expect("GPS - uart driver error");
+        )
+        .expect("GPS - uart driver error");
 
+        // Disable a NMEA messages
+        let init_commands = [
+            [
+                0xF1, 0xD9, 0x06, 0x01, 0x03, 0x00, 0xF0, 0x01, 0x00, 0xFB, 0x11,
+            ],
+            [
+                0xF1, 0xD9, 0x06, 0x01, 0x03, 0x00, 0xF0, 0x02, 0x00, 0xFC, 0x13,
+            ],
+            [
+                0xF1, 0xD9, 0x06, 0x01, 0x03, 0x00, 0xF0, 0x04, 0x00, 0xFE, 0x17,
+            ],
+            [
+                0xF1, 0xD9, 0x06, 0x01, 0x03, 0x00, 0xF0, 0x06, 0x00, 0x00, 0x1B,
+            ],
+            [
+                0xF1, 0xD9, 0x06, 0x01, 0x03, 0x00, 0xF0, 0x07, 0x00, 0x01, 0x1D,
+            ],
+            [
+                0xF1, 0xD9, 0x06, 0x01, 0x03, 0x00, 0xF0, 0x08, 0x00, 0x02, 0x1F,
+            ],
+            [
+                0xF1, 0xD9, 0x06, 0x01, 0x03, 0x00, 0xF0, 0x20, 0x00, 0x1A, 0x4F,
+            ],
+        ];
+
+        for (i, command) in init_commands.iter().enumerate() {
+            match uart.write(command) {
+                Ok(_) => println!("GPS initialization command {} sent successfully", i + 1),
+                Err(e) => println!(
+                    "Failed to send GPS initialization command {}: {:?}",
+                    i + 1,
+                    e
+                ),
+            }
+            thread::sleep(Duration::from_millis(100));
+        }
+
+        // Set NMEA output frequency 10Hz
+        let init_command = [
+            0xF1, 0xD9, 0x06, 0x42, 0x14, 0x00, 0x00, 0x0A, 0x38, 0x00, 0x64, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0x24,
+        ];
+
+        match uart.write(&init_command) {
+            Ok(_) => println!("GPS initialization command sent successfully"),
+            Err(e) => println!("Failed to send GPS initialization command: {:?}", e),
+        }
+        thread::sleep(Duration::from_millis(100));
 
         let mut nmea_buffer = String::with_capacity(100);
         let mut buf = [0_u8; 1];
@@ -37,7 +86,7 @@ impl Gps {
             nmea_buffer.push(char::from(buf[0]));
 
             if nmea_buffer.ends_with("\r\n") {
-                if nmea_buffer.contains("GNGGA")||nmea_buffer.contains("GNRMC") {
+                if nmea_buffer.contains("GNGGA") || nmea_buffer.contains("GNRMC") {
                     println!("{}", nmea_buffer.yellow());
                     tx.send(nmea_buffer.clone())
                         .expect("Can't send from GPS thread");
@@ -51,5 +100,4 @@ impl Gps {
 
         Gps { rx }
     }
-
 }
--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,7 @@ fn main() {
     esp_idf_hal::sys::link_patches();
     esp_idf_svc::log::EspLogger::initialize_default();
 
-
     let peripherals = Peripherals::take().expect("GPS - can't take peripherals");
-
 
     println!("Start NMEA listener");
     let uart1 = peripherals.uart1;
@@ -23,7 +21,6 @@ fn main() {
     let rx = peripherals.pins.gpio17;
     let gps = Gps::new(uart1, AnyIOPin::from(tx), AnyIOPin::from(rx));
     println!("----------------------------------------------------");
-
 
     println!("Start BLE");
     let mut ble = Ble::new("AGRO-GPS");
@@ -34,8 +31,6 @@ fn main() {
 
             ble.send(&nmea);
         }
-
-
 
         // we are using thread::sleep here to make sure the watchdog isn't triggered
         FreeRtos::delay_ms(10);
